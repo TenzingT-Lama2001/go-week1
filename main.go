fd4a37ac45ba7003d721c649a899fef147a85d84
TenzingT-Lama2001 <tenzing.lama36@gmail.com>
2024-01-11 11:02:32 +0545

    Add: Database connetion

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
-// main.go
-
 package main
 
 import (
 	"log"
 	"net"
-
+	"time"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 	"week1/app/blog/api"
@@ -13,22 +13,47 @@ import (
 	"week1/app/blog/service"
 )
 
+// App struct to hold dependencies like the database
+type App struct {
+	DB *sql.DB
+}
+
 func main() {
+	db, err := sql.Open("mysql", "root:Password!?@#$123@tcp(localhost:3306)/blogs")
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	// Create an instance of the App struct
+	app := &App{DB: db}
+
+	// Start gRPC server
+	go startGRPCServer(app)
+
+	// Start HTTP server
+	startHTTPServer()
+}
+
+func startGRPCServer(app *App) {
 	lis, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
 	serverRegistry := grpc.NewServer()
-	blogGRPCService := service.NewBlogGRPCService()
+	blogGRPCService := service.NewBlogGRPCService(app.DB)
 	pb.RegisterBlogServiceServer(serverRegistry, blogGRPCService)
 
-	go func() {
-		if err := serverRegistry.Serve(lis); err != nil {
-			log.Fatalf("failed to serve gRPC server: %v", err)
-		}
-	}()
+	if err := serverRegistry.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC server: %v", err)
+	}
+}
 
+func startHTTPServer() {
 	// Create a gRPC client
 	conn, err := grpc.Dial(":8080", grpc.WithInsecure())
 	if err != nil {
@@ -39,7 +64,6 @@ func main() {
 	// Create the gRPC client
 	blogServiceClient := pb.NewBlogServiceClient(conn)
 
-	// Create the API and inject the gRPC client
 	blogAPI := api.NewBlogController(blogServiceClient)
 
 	// Create a new gin router
